ebd0f5c4553b03e52cbafd4975569b44433debf7
Gnelezie <zie.nanien@gmail.com>
2022-04-18 14:35:29 +0000

    Mise à jour

---
+++ layout/footer.php
@@ -0,0 +1,67 @@
+
+	<footer class="footer-six">
+		<div class="overlay">
+			<div class="container">
+				<ul class="event-social">
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+				</ul>
+				<p>Copyright &copy; 2022. Design and Development by <a href="#">Gnelezie</a>
+				</p>
+			</div>
+		</div>
+	</footer>
+	<script src="<?=$asset?>/js/jquery-3.1.1.min.js"></script>
+	<script src="<?=$asset?>/js/jquery-migrate-1.4.1.min.js"></script>
+
+	<!-- Bootstrap -->
+	<script src="<?=$asset?>/js/bootstrap.min.js"></script>
+
+	<!-- Coundown -->
+	<script src="<?=$asset?>/js/jquery.countdown.min.js"></script>
+
+	<!--Swiper-->
+	<script src="<?=$asset?>/js/swiper.jquery.min.js"></script>
+
+	<!--Masonry-->
+	<script src="<?=$asset?>/js/masonry.pkgd.min.js"></script>
+
+	<!--Lightcase-->
+	<script src="<?=$asset?>/js/lightcase.js"></script>
+
+	<!--modernizr-->
+	<script src="<?=$asset?>/js/modernizr.js"></script>
+
+	<!--velocity-->
+	<script src="<?=$asset?>/js/velocity.min.js"></script>
+
+	<!--quick-view-->
+	<script src="<?=$asset?>/js/quick-view.js"></script>
+
+	<!--nstSlider-->
+	<script src="<?=$asset?>/js/jquery.nstSlider.js"></script>
+	<script src="<?=$asset?>/js/nstfunctions.js"></script>
+
+	<!--flexslider-->
+	<script src="<?=$asset?>/js/flexslider-min.js"></script>
+	<script src="<?=$asset?>/js/flexfunctions.js"></script>
+
+	<!--directional-->
+	<script src="<?=$asset?>/js/directional-hover.js"></script>
+	<!-- parallax.js -->
+	<script src="<?=$asset?>/js/parallax.js"></script>
+	<script src="<?=$asset?>/js/theia-sticky-sidebar.js"></script>
+
+	<!--easing-->
+	<script src="<?=$asset?>/js/jquery.easing.min.js"></script>
+
+
+
+
+</body>
+
+</html>
---
+++ layout/header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AEEK - KASSERE</title>
+
+    <link rel="icon" href="<?=$asset?>/media/logoAEEK.png" type="image/x-icon">
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i%7CPoppins:300,400,500,600,700"
+		rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Archivo:400,500,600,700|Oswald:200,300,400,500,600,700"
+		rel="stylesheet">
+	<link href="<?=$asset?>/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/font-awesome.min.css" rel="stylesheet">
+	<link href="<?=$asset?>/flaticon/flaticon.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/swiper.min.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/lightcase.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/quick-view.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/jquery.nstSlider.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/flexslider.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/banner-bg.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/style.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/header.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/index-9.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/responsive.css" rel="stylesheet">
+    <link href="<?=$asset?>/css/headers.css" rel="stylesheet">
+    <link href="<?=$asset?>/css/font-size.css" rel="stylesheet">
+	<link href="<?=$asset?>/css/btn.css" rel="stylesheet">
+
+
+<style>
+.banner{
+	height: 600px !important;
+	}
+.bg-white{
+    background:#fff !important;
+}
+header.style-3 div.menu-fixed::after{
+    background : #ff460000 !important;
+}
+
+header.style-3 div.menu-fixed::before {
+    border-bottom: 90px solid #ff460000;
+}
+.banner-11 .right-content img {
+    transform: translateX(1px) !important;
+}
+.side-image2:before {
+    background-image: url(<?=$asset?>/media/right1.png) !important;
+}
+
+.side-image2:after {
+    background-image: url(<?=$asset?>/media/left1.png) !important;
+}
+.multi-gallery-section.style2 {
+    background-image: url(<?=$asset?>/media/bg.jpg);
+}
+
+
+.menu-button{
+	padding: 5px 20px !important;
+    text-transform: inherit !important;
+}
+
+@media(max-width: 767px) {
+    nav.navbar{
+        background:#f8f9fa!important;
+    }
+    .banner-11 .right-content img {
+        transform: translateY(80px) !important;
+    }
+    .banner {
+        height: 339px !important;
+    }
+	.banner .banner-overlay {
+    height: 249px !important;
+}
+}
+
+
+
+</style>
+
+
+
+</head>
+
+<body class="home-fashion">
+	<header class="style-3">
+		<div class="menu-fixed p-0">
+			<nav class="navbar navbar-expand-lg bg-white">
+				<div class="container">
+					<a class="navbar-brand" href="index.html"><img src="<?=$asset?>/media/logoAEEK.png" width="50" alt="logo"
+							class="img-responsive"></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+						aria-expanded="false" aria-label="Toggle navigation">
+						<i class="fa fa-bars" style="color: #000;"></i>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<div class="main-menu" style="">
+							<div class="menu-left">
+								<ul>						
+                                <li><a href="">Accueil</a></li>
+                                <li><a href="">Bureau</a></li>
+
+                                <li><a href="">A propos</a></li>
+
+                                <li><a href="">Contact</a></li>
+
+                                <li><a href="">Blog</a></li>
+                                <li><a href="">Aeek tv</a></li>
+
+								</ul>
+							</div>
+						</div>
+                        <div class="main-menu">
+						
+							<div class="menu-right">
+								<ul class="header-cart-ticket-option">
+									<li><a href="ticket.html" class="menu-button btn-transparence-orange">Inscription</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!-- header end here -->
